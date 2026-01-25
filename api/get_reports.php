3a73d6b5a2afc1b2974e4e75ef48656ad6d187d1
hafidzmw <hafidzmerdiowidjayanta@mail.ugm.ac.id>
2026-01-25 17:10:26 +0700

    update backend

--- api/get_reports.php
+++ api/get_reports.php
@@ -1,5 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+// Handle preflight (penting!)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $pdo = new PDO(
   "mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'),
