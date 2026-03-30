cac0984d2a8891053cb27a50d531a1474f5db012
hafidzmw <hafidzmerdiowidjayanta@mail.ugm.ac.id>
2026-03-30 19:02:38 +0700

    bismillah semoga bisa

--- execution-code.py
+++ execution-code.py
@@ -8,7 +8,7 @@ import time
 app = FastAPI()
 
 # 1. Path untuk kedua model
-MODEL_H5_FILE = os.getenv("MODEL_PATH", "model/model_dnn300326.keras")
+MODEL_H5_FILE = os.getenv("MODEL_PATH", "model/model_dnn300326.h5")
 MODEL_TFLITE_FILE = os.getenv("MODEL_TINY_PATH", "model/model_dnntiny300326.tflite")
 
 # 2. Variabel Global
